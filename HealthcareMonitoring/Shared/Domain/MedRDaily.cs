addc7bea8be0359788935a6995395d9cced1bf30
Jia Wei <148171957+jiawei3311@users.noreply.github.com>
2024-01-19 22:13:21 +0800

    new

---
+++ HealthcareMonitoring/Shared/Domain/MedRDaily.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthcareMonitoring.Shared.Domain
+{
+	internal class MedRDaily
+	{
+	}
+}
---
+++ HealthcareMonitoring/Shared/Domain/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthcareMonitoring.Shared.Domain
+{
+	public class Patient : BaseDomainModel
+	{
+		public Boolean Allergies { get; set; }
+
+		public string? AllergiesDescription { get; set; }
+
+
+
+	}
+}
