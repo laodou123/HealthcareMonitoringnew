237cfc28428bde48c652ea37251d02f36da04331
Argo-AscioTech <argo@live.ca>
2024-01-19 21:44:39 +0800

    refactor: 更新代码能运行

--- HealthcareMonitoring/Server/Data/ApplicationDbContext.cs
+++ HealthcareMonitoring/Server/Data/ApplicationDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Duende.IdentityServer.EntityFramework.Options;
 using HealthcareMonitoring.Server.Models;
-using HealthcareMonitoring.Shared.Domain;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -8,9 +7,6 @@ using HealthcareMonitoring.Shared.Domain;
 
 namespace HealthcareMonitoring.Server.Data
 {
-    public DbSet<Doctor> Doctors { get; set; }
-    public DbSet<Diagnosis> Diagnoses { get; set; }
-    public DbSet<Prescription> Prescriptions { get; set; }
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
         public ApplicationDbContext(
