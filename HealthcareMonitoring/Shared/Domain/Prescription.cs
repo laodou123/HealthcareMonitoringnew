81b82ac18e924dccdf68962d564f6ac5b41b5720
Argo-AscioTech <argo@live.ca>
2024-01-22 19:36:01 +0800

    feat: 更新一些小细节

--- HealthcareMonitoring/Shared/Domain/Prescription.cs
+++ HealthcareMonitoring/Shared/Domain/Prescription.cs
@@ -1,11 +1,27 @@
-﻿namespace HealthcareMonitoring.Shared.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthcareMonitoring.Shared.Domain
 {
     public class Prescription : BaseDomainModel
     {
+        [Display(Name = "Name")]
+        [Required]
         public string? MedicineName { get; set; }
+
+        [Display(Name = "Quantity")]
+        [Required]
         public int? MedicineQuantity { get; set; }
+
+        [Display(Name = "Description")]
+        [Required]
         public string? MedicineDescription { get; set; }
+
+        [Display(Name = "Usage")]
+        [Required]
         public string? MedicineUsage { get; set; }
+
+        [Display(Name = "Doctor")]
+        [Required]
         public string? MedicinePrescriptionDoctor { get; set; }
     }
 }
