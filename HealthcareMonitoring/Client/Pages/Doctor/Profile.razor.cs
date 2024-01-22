3f5e47f379b0171a2f01b20d726aeb74410847db
Argo-AscioTech <argo@live.ca>
2024-01-22 19:36:01 +0800

    feat: 更新一些小细节

--- HealthcareMonitoring/Client/Pages/Doctor/Profile.razor.cs
+++ HealthcareMonitoring/Client/Pages/Doctor/Profile.razor.cs
@@ -1,42 +1,23 @@
+using HealthcareMonitoring.Shared.Domain;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
 using System.Diagnostics.CodeAnalysis;
 
 namespace HealthcareMonitoring.Client.Pages.Doctor;
 
 public partial class Profile
 {
-    private string? _profileString;
-
-    [Inject]
-    [NotNull]
-    private AuthenticationStateProvider? AuthenticationStateProvider { get; set; }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    protected override void OnInitialized()
-    {
-        base.OnInitialized();
-
-        // TODO: 应该从数据库中获取医生的 Profile 信息赋值给变量即可
-        _profileString = "<h1>我是医生</h1>";
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    /// <returns></returns>
-    protected override async Task OnInitializedAsync()
-    {
-        var state = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-        var iden = state.User.Identity;
-        var docter = state.User.IsInRole("Doctor");
-    }
+    private HealthcareMonitoring.Shared.Domain.Doctor _doctor = new();
 
     private Task OnSaveProfileAsync(string profileString)
     {
         // TODO: 保存到数据库中
+        return Task.CompletedTask;
+    }
+
+    private Task OnSubmit(EditContext context)
+    {
         return Task.CompletedTask;
     }
 }
--- HealthcareMonitoring/Shared/Domain/Doctor.cs
+++ HealthcareMonitoring/Shared/Domain/Doctor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,14 @@ namespace HealthcareMonitoring.Shared.Domain
 {
     public class Doctor : BaseDomainModel
     {
+        [Display(Name = "Name")]
+        [Required]
         public string? DoctorName {  get; set; }
+
+        [Display(Name = "PhoneNumber")]
         public int? DoctorPhoneNumber {  get; set; }
 
+        [Display(Name = "Specialization")]
         public string? DoctorSpecialization {  get; set; }
 
         public DateTime? DoctorAvailavleTime {  get; set; }
