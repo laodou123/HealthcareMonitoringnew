1a7e13f1f8a8c4f2ef82c6f9f8805a70882271f9
Jia Wei <148171957+jiawei3311@users.noreply.github.com>
2024-01-28 19:15:03 +0800

    improve appointment page. BUT still cannot submit

--- HealthcareMonitoring/Server/Migrations/20240128102341_addednewdoc.Designer.cs
+++ HealthcareMonitoring/Server/Migrations/20240128102341_addednewdoc.Designer.cs
@@ -4,6 +4,7 @@ using HealthcareMonitoring.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HealthcareMonitoring.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240128102341_addednewdoc")]
+    partial class addednewdoc
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
