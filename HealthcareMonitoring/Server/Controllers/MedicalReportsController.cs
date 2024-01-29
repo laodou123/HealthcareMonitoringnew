c2e6374a1c6bcc79550b17f51b2e69dc0b83f42c
Argo-AscioTech <argo@live.ca>
2024-01-29 10:08:20 +0800

    refactor: 精简 webapi 命名空间

--- HealthcareMonitoring/Server/Controllers/MedicalReportsController.cs
+++ HealthcareMonitoring/Server/Controllers/MedicalReportsController.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HealthcareMonitoring.Server.IRepository;
-using Microsoft.AspNetCore.Http;
+﻿using HealthcareMonitoring.Server.IRepository;
+using HealthcareMonitoring.Shared.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using HealthcareMonitoring.Server.Data;
-using HealthcareMonitoring.Server.IRepository;
-using HealthcareMonitoring.Shared.Domain;
 
 namespace HealthcareMonitoring.Server.Controllers
 {
